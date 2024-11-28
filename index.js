d00e3c55d5f42b4a6c51281a41e565c42d647d3e
Sergio <chechocardona75@gmail.com>
2024-11-27 21:20:56 -0500

    Función de agregar, editar y eliminar añadidas en la vista.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
     console.log('Middleware');
--- routes/mangas.views.router.js
+++ routes/mangas.views.router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { index } from "../services/mangas.services.js";
+import { index, create, destroy, update } from "../services/mangas.services.js";
 export const mangasViewsRouter = express.Router();
 
 mangasViewsRouter.get("/", async (req, res) => {
@@ -7,4 +7,23 @@ mangasViewsRouter.get("/", async (req, res) => {
     res.render('index', {
         mangas
     });
+})
+
+mangasViewsRouter.post("/", async (req, res) => {
+    let { titulo, autor, genero, volumenes, fechaPublicacion, sinopsis, calificacion, editorial } = req.body;
+    await create ({ titulo, autor, genero, volumenes, fechaPublicacion, sinopsis, calificacion, editorial });
+    res.redirect("/mangas");
+})
+
+mangasViewsRouter.post("/edit/:id", async (req, res) => {
+    const { id } = req.params;
+    let { titulo, autor, genero, volumenes, fechaPublicacion, sinopsis, calificacion, editorial } = req.body;
+    await update (id, {titulo, autor, genero, volumenes, fechaPublicacion, sinopsis, calificacion, editorial});
+    res.redirect("/mangas");
+})
+
+mangasViewsRouter.post("/destroy/:id", async (req, res) => {
+    const { id } = req.params;
+    await destroy (id);
+    res.redirect("/mangas");
 })
